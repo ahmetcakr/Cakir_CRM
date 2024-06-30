abbb8c76b4952db4561c2acc8b68e69aca7ce612
Ahmet ÇAKIR <ahmetcaakirr@gmail.com>
2024-06-30 13:54:49 +0300

    Entities about Company added and nuget upgraded

---
+++ src/CRM/CRM.Domain/Entities/Companies.cs
@@ -0,0 +1,37 @@
+﻿namespace CRM.Domain.Entities;
+
+public class Companies 
+{
+    public int Id { get; set; }
+    public string CompanyName { get; set; }
+    public int TypeId { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+    public virtual CompanyTypes CompanyTypes { get; set; }
+
+    public Companies()
+    {
+        Id = 0;
+        CompanyName = string.Empty;
+        TypeId = 0;
+        Address = string.Empty;
+        Phone = string.Empty;
+        Email = string.Empty;
+        Website = string.Empty;
+    }
+
+    public Companies(int id, string companyName, int typeId, string address, string phone, string email, string website)
+    {
+        Id = id;
+        CompanyName = companyName;
+        TypeId = typeId;
+        Address = address;
+        Phone = phone;
+        Email = email;
+        Website = website;
+    }
+
+
+}
