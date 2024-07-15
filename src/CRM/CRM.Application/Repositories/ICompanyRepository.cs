de1a94ae180f5588d19cb85c73fe2321c6d55f89
Ahmet ÇAKIR <ahmetcaakirr@gmail.com>
2024-07-15 17:08:42 +0300

    Company Configuration and Repository

---
+++ src/CRM/CRM.Application/Repositories/ICompanyRepository.cs
@@ -0,0 +1,6 @@
+﻿using Core.Persistence.Repositories;
+using CRM.Domain.Entities;
+
+namespace CRM.Application.Repositories;
+
+public interface ICompanyRepository : IAsyncRepository<Company, int>, IRepository<Company, int> { }
---
+++ src/CRM/CRM.Persistence/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CRM.Persistence.Configurations;
+
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable("Companies").HasKey(c => c.Id);
+
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).HasColumnName("Id").IsRequired();
+        builder.Property(c => c.CompanyName).HasColumnName("CompanyName").IsRequired();
+        builder.Property(c => c.CompanyTypeId).HasColumnName("CompanyTypeId").IsRequired();
+
+        builder.HasOne(c => c.CompanyType)
+            .WithMany()
+            .HasForeignKey(c => c.CompanyTypeId);
+    }
+}
