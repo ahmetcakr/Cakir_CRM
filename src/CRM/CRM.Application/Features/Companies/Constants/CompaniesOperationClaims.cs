2107e9320d4f9069c711ae8d5b5a5566dc4013c4
Ahmet ÇAKIR <ahmetcaakirr@gmail.com>
2024-07-15 18:41:16 +0300

    Companies Service

---
+++ src/CRM/CRM.Application/Features/Companies/Constants/CompaniesOperationClaims.cs
@@ -0,0 +1,13 @@
+﻿namespace CRM.Application.Features.Companies.Constants;
+
+public class CompaniesOperationClaims
+{
+    public const string Admin = "companies.admin";
+
+    public const string Read = "companies.read";
+    public const string Write = "companies.write";
+
+    public const string Add = "companies.add";
+    public const string Update = "companies.update";
+    public const string Delete = "companies.delete";
+}
---
+++ src/CRM/CRM.Application/Services/CompaniesService/CompanyManager.cs
@@ -0,0 +1,53 @@
+﻿using Core.Persistence.Paging;
+using CRM.Application.Features.Companies.Rules;
+using CRM.Application.Repositories;
+using CRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace CRM.Application.Services.CompaniesService;
+
+public class CompanyManager(
+    ICompanyRepository companyRepository,
+    CompanyBusinessRules companyBusinessRules) : ICompanyService
+{
+    public async Task<Company> AddAsync(Company company)
+    {
+        await companyBusinessRules.CompanyNameShouldNotExistsWhenInsert(company.CompanyName);
+
+        return await companyRepository.AddAsync(company);
+    }
+
+    public async Task<Company> DeleteAsync(Company company, bool permanent = false)
+    {
+        return await companyRepository.DeleteAsync(company, permanent);
+    }
+
+    public async Task<Company?> GetAsync(Expression<Func<Company, bool>> predicate, Func<IQueryable<Company>, IIncludableQueryable<Company, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        var Companies = await companyRepository.GetAsync(predicate, include, withDeleted, enableTracking, cancellationToken);
+        return Companies;
+    }
+
+    public async Task<IPaginate<Company?>?> GetListAsync(Expression<Func<Company, bool>>? predicate = null, Func<IQueryable<Company>, IOrderedQueryable<Company>>? orderBy = null, Func<IQueryable<Company>, IIncludableQueryable<Company, object>>? include = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        IPaginate<Company> companyList = await companyRepository.GetListAsync(
+            predicate,
+            orderBy,
+            include,
+            index,
+            size,
+            withDeleted,
+            enableTracking,
+            cancellationToken
+        );
+
+        return companyList;
+    }
+
+    public async Task<Company> UpdateAsync(Company request)
+    {
+        Company company = await companyRepository.UpdateAsync(request);
+        return company;
+    }
+}
